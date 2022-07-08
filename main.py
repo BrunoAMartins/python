f376c2cb39fc06b3a05bbcd840f8426e3ac03b3f
BrunoA.Martins <108954323+BrunoAMartins@users.noreply.github.com>
2022-07-08 17:47:02 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+print("Bem vindo ao gerador de nome de banda")
+
+city = input("Qual o nome da cidade a qual voce nasceu \n")
+
+pet = input("qual o nome do seu primeiro animal de estimacao \n")
+
+print("o nome da sua banda e " + city +" "+ pet)
+
